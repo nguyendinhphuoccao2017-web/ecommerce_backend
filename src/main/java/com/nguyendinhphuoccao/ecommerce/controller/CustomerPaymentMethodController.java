ba4c9a824128f962c070b8c69a891ce1bb33ab92
nguyendinhphuoccao2017-web <nguyendinhphuoccao2017@gmail.com>
2026-06-19 23:31:12 +0700

    Payment

--- src/main/java/com/nguyendinhphuoccao/ecommerce/controller/CustomerPaymentMethodController.java
+++ src/main/java/com/nguyendinhphuoccao/ecommerce/controller/CustomerPaymentMethodController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/payment-methods")
+@RequestMapping("/api/payment-methods")
 @RequiredArgsConstructor
 public class CustomerPaymentMethodController {
 
